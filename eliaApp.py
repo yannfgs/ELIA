d2daedbed29bf3bcd9e1ee4dcfde0fdc633271be
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-11-23 08:28:26 -0300

    23112302

--- eliaApp.py
+++ eliaApp.py
@@ -2,17 +2,16 @@ import tkinter as tk
 from tkinter import messagebox, scrolledtext
 import requests
 
-# Substitua pela sua chave de API e Organization ID reais da OpenAI
-API_KEY = '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'
-ORGANIZATION_ID = 'org-4GGvTGan5YuCScHmLKDtIGt8'
+# Substitua pela sua chave de API real da OpenAI
+API_KEY = 'vgBwZU9MeRPukewGxEpiT3BlbkFJ3glWxR8VEEmHt7tDtKyX'
 
 class EliaApp:
     def __init__(self, master):
         self.master = master
-        master.title("Aplicação ELIA - ChatGPT Personalizado")
+        master.title("Aplicação ELIA - ChatGPT com OpenAI")
 
         # Label e entrada de texto para a mensagem do usuário
-        self.label_message = tk.Label(master, text="Digite sua mensagem para a ELIA:")
+        self.label_message = tk.Label(master, text="Digite sua mensagem para o assistente:")
         self.label_message.pack()
 
         self.text_message = tk.Entry(master, width=50)
@@ -23,7 +22,7 @@ class EliaApp:
         self.button_send.pack()
 
         # Área de texto com barra de rolagem para a resposta
-        self.label_response = tk.Label(master, text="Resposta da ELIA:")
+        self.label_response = tk.Label(master, text="Resposta do assistente:")
         self.label_response.pack()
 
         self.text_response = scrolledtext.ScrolledText(master, height=15, width=50)
@@ -32,21 +31,20 @@ class EliaApp:
     def send_message(self):
         prompt = self.text_message.get()
         data = {
-            'input': {
-                'text': prompt.strip(),
-            }
+            'model': 'gpt-3.5-turbo',  # Escolha o modelo adequado para o seu caso
+            'messages': [
+                {"role": "system", "content": "Você é um assistente útil."},
+                {"role": "user", "content": prompt.strip()}
+            ]
         }
 
         headers = {
             'Authorization': f'Bearer {API_KEY}',
-            'OpenAI-Organization': ORGANIZATION_ID
+            'Content-Type': 'application/json'
         }
 
-        assistant_id = 'asst_9ME2sTydFVlaMbHADWoI0nu9'  # Substitua pelo ID do seu assistente personalizado
-
         try:
-            url = f'https://api.openai.com/v1/assistants/{assistant_id}/messages'
-            response = requests.post(url, headers=headers, json=data)
+            response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=data)
             response.raise_for_status()
             response_data = response.json()
             self.text_response.delete(1.0, tk.END)
